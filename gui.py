d506aaf1e57e6758483a2d33c0db483d03167d94
Oscar Garcia <oegz03@gmail.com>
2024-09-19 17:41:41 -0600

    Atributo del lienzo

--- gui.py
+++ gui.py
@@ -1,7 +1,11 @@
 from tkinter import Tk
+from tkinter import Canvas
 
 class Interfaz:
     def __init__(self, simulacion):
         self.simulacion = simulacion
         self.tk = Tk()
         self.tk.title("Simulación de Partículas")
+
+        self.lienzo = Canvas(self.tk, width=self.simulacion.ancho, height=self.simulacion.alto)
+        self.lienzo.pack()
