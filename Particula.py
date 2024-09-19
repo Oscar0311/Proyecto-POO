b51d3fd8765ca3271055ffb9b7868a12fe58f528
Luis Saavedra <saa24822@uvg.edu.gt>
2024-09-19 17:28:10 -0600

    Creacion de la clase particula y su constructor respectivo

---
+++ Particula.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class Particula:
+    def __init__(self, simulacion, x, y, radio=4, color='random', masa=1, velocidad=np.zeros(2), rebote=0.7):
+        self.simulacion = simulacion
+        self.x = x
+        self.y = y
+        self.radio = radio
+        self.color = np.random.randint(0, 255, 3).tolist() if color == 'random' else color
+        self.masa = masa
+        self.velocidad = np.array(velocidad).astype('float32')
+        self.aceleracion = np.zeros(2)
+        self.rebote = rebote
+        self.radio_atraccion = 50
+        self.radio_repulsion = 10
+        self.atraccion = 0.5
+        self.repulsion = 1
