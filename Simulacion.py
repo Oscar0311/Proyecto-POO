c23ffda14b58436824aa85eccf8f49e56a1e6110
Marcelo Ixquiac <ixq24712@uvg.edu.gt>
2024-09-19 17:29:41 -0600

    Desarrollo Class Simulacion

---
+++ Simulacion.py
@@ -0,0 +1,19 @@
+import numpy as np
+from Particula import *
+
+
+class Simulacion:
+    def _init_(self, ancho=650, alto=600):
+        self.ancho = ancho
+        self.alto = alto
+        self.particulas = []
+        self.pausado = False
+        self.velocidad = 0.1
+        self.temperatura = 0.5
+        self.vector_g = np.array([0, 0.1])
+        self.fuerza_viento = np.array([0, 0])
+        self.res_aire = 0.05
+        self.friccion_suelo = 0.05
+    
+    def fuerza_aplicada(self, fuerza):
+        self.a += fuerza / float(abs(self.m)) 
