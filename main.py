18e64c2230a3f180f5a2156754f965e588a77e5a
Marcelo Ixquiac <ixq24712@uvg.edu.gt>
2024-09-19 22:16:17 -0600

    Ciclo Particulas y muestreo inerfaz

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 from gui import *
 from Particula import *
 from Simulacion import *
@@ -5,3 +6,10 @@ from Simulacion import *
 if __name__ == "__main__":
     simulacion = Simulacion()
     interfaz = Interfaz(simulacion)
+    
+    for _ in range(50):
+        x = random.uniform(50, simulacion.ancho - 50)
+        y = random.uniform(50, simulacion.alto - 50)
+        simulacion.agregar_particula(x, y)
+
+    interfaz.ejecutar()
