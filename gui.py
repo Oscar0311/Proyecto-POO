3af2c3b70bd8b38943c95132c3ebef4561be5856
Oscar Garcia <oegz03@gmail.com>
2024-09-19 20:07:45 -0600

    Sets de los atributos del objeto "simulacion".

--- gui.py
+++ gui.py
@@ -9,3 +9,18 @@ class Interfaz:
 
         self.lienzo = Canvas(self.tk, width=self.simulacion.ancho, height=self.simulacion.alto)
         self.lienzo.pack()
+
+    def set_gravedad(self, valor):
+        self.simulacion.vector_g[1] = float(valor)
+
+    def set_resistencia_aire(self, valor):
+        self.simulacion.res_aire = float(valor)
+
+    def set_temperatura(self, valor):
+        self.simulacion.temperatura = float(valor)
+
+    def set_friccion_suelo(self, valor):
+        self.simulacion.friccion_suelo = float(valor)
+
+    def set_velocidad(self, valor):
+        self.simulacion.velocidad = float(valor)
