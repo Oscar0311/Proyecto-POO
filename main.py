ec45bd5c0e291819b2d8bb66e1bd96108ddd6692
Aldoriks <bol241179@uvg.edu.gt>
2024-11-14 22:04:35 -0600

    arreglo fisicas parte 6?

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     interfaz = Interfaz(simulacion)
 
     # Añadir algunas partículas iniciales a la simulación de forma aleatoria
-    for i in range(10):
+    for i in range(50):
         x = random.uniform(50, simulacion.get_ancho() - 50)
         y = random.uniform(50, simulacion.get_alto() - 50)
         simulacion.agregar_particula(x, y, 4, 1, 0.7, np.zeros(2))
